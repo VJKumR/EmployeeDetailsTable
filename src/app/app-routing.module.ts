1ed741faa98195e42c6139eedff6e1839a72e1bb
vjk1729 <pvijay1999kumar@gmail.com>
2021-02-20 13:42:47 +0530

    Changes made.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,10 @@
-import { UserDataComponent } from './components/employee-data/user-data.component';
+import { EmployeeDataComponent } from './components/employee-data/employee-data.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
-  { path: 'home', component: UserDataComponent },
+  { path: 'home', component: EmployeeDataComponent },
 ];
 
 @NgModule({
